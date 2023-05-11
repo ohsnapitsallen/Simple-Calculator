91425349a06a22fc601edd58898946692a40ed39
John Allen Lorena <johnallenlorena@gmail.com>
2023-05-11 22:26:35 +0800

    User inputs added

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,7 +1,11 @@
 #Create a function for the calculator
 def calculate():
     print("Let's calculate!")
-#Use try method to ask the user for numbers and operators
+    #Use try method to ask the user for numbers and operators
+    try:
+        first = float(input("Enter any number (Can be a whole number or a decimal): "))
+        op = str(input('Enter an operator (Must be plus "+", minus "-", times "*", or divide "/": '))
+        second = float(input("Enter another number (Can be a whole number or a decimal): "))
 #Use if statements for solving the two numbers depending on the operator that user has inputted
 #Create error messages if user's input are invalid or user divides with zero
 #Create an empty variable for the while loop to be used later
