a4fcdb6a3fd772c5c37bb9c3ff021fbe4e596fd9
John Allen Lorena <johnallenlorena@gmail.com>
2023-05-11 22:23:25 +0800

    Pseudocodes

---
+++ Simple Calculator.py
@@ -0,0 +1,8 @@
+#Create a function for the calculator
+#Use try method to ask the user for numbers and operators
+#Use if statements for solving the two numbers depending on the operator that user has inputted
+#Create error messages if user's input are invalid or user divides with zero
+#Create an empty variable for the while loop to be used later
+#Print a welcome message
+#Use the while loop to ask the user if they would like to use the program again
+#Use if statements depending on the user's response
